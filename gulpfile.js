1086e8437ad4c29e2204a83b5004b4d9fa09fe05
Alex Klyuchnik <sowedoweb@gmail.com>
2018-01-24 18:47:20 +0200

    still preparing

--- gulpfile.js
+++ gulpfile.js
@@ -48,8 +48,8 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('sass', function() {
-	return gulp.src('app/sass/**/*.sass')
-	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+	return gulp.src('app/sass/**/*.scss')
+	.pipe(sass({outputStyle: 'nest'}).on("error", notify.onError()))
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer(['last 15 versions']))
 	/*.pipe(cleanCSS())*/ // Опционально, закомментировать при отладке
